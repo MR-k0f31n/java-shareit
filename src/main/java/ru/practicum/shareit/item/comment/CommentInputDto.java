9cc22212de4be21ca522f479afd398cbce1ec893
MR-k0f31n <bukalov_igor@mail.ru>
2023-06-06 17:41:26 +0300

    <Add> test user successfully <Next> items test and other

--- src/main/java/ru/practicum/shareit/item/comment/CommentInputDto.java
+++ src/main/java/ru/practicum/shareit/item/comment/CommentInputDto.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.item.comment;
 
-import lombok.Getter;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull;
 /**
  * @author MR.k0F31n
  */
-@Getter
+@Data
 public class CommentInputDto {
     @NotNull(message = "Comments is null!")
     @NotBlank(message = "Comments is empty!")
