85bbc6d14ed1d8e3f7d23a51a2076a41bb580747
MR-k0f31n <bukalov_igor@mail.ru>
2023-06-07 23:59:03 +0300

    <Add> test user successfully <Next> items test and other

--- src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
@@ -10,14 +10,6 @@ import java.util.List;
  * @author MR.k0F31n
  */
 public class ItemRequestMapper {
-    public static ItemRequest dtoToObject(ItemRequestDto dto) {
-        return new ItemRequest(
-                dto.getId(),
-                dto.getDescription(),
-                new User(),
-                dto.getCreated()
-        );
-    }
 
     public static ItemRequest dtoToObject(ItemRequestInputDto dto) {
         return new ItemRequest(
@@ -44,14 +36,6 @@ public class ItemRequestMapper {
                 request.getCreatedDate(),
                 new ArrayList<>()
         );
-    }
-
-    public static List<ItemRequestDto> objectToDtoList(Iterable<ItemRequest> iterable) {
-        List<ItemRequestDto> result = new ArrayList<>();
-        for (ItemRequest item : iterable) {
-            result.add(objectToDto(item));
-        }
-        return result;
     }
 
     public static List<ItemRequestWithAnswerDto> objectToDtoListWithAnswer(Iterable<ItemRequest> iterable) {
