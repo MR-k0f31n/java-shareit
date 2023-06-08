c00438f3d6156c8ca17613ea84bc6127751cd154
MR-k0f31n <bukalov_igor@mail.ru>
2023-06-08 12:32:05 +0300

    <Add> test user successfully <Next> items test and other

--- src/test/java/ru/practicum/shareit/user/UserServiceWithcContext.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceWithcContext.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.practicum.shareit.booking.BookingInputDto;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.ItemInputDto;
@@ -12,8 +13,7 @@ import ru.practicum.shareit.item.ItemService;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -41,6 +41,80 @@ public class UserServiceWithcContext {
     }
 
     @Test
+    void updateUser_nameIsBlank_notUpdate() {
+        UserDto user = service.createNewUser(user1);
+
+        UserDto updateUser = user1;
+        updateUser.setName("");
+
+        UserDto userBeforeUpdate = service.updateUser(updateUser, user.getId());
+
+        assertEquals("name1", userBeforeUpdate.getName());
+    }
+
+    @DirtiesContext
+    @Test
+    void updateUser_nameIsNull_notUpdate() {
+        UserDto userOne = new UserDto(1L, "name1", "emai11120@mail.com");
+        UserDto user = service.createNewUser(userOne);
+
+        UserDto updateUser = userOne;
+        updateUser.setName(null);
+
+        UserDto userBeforeUpdate = service.updateUser(updateUser, user.getId());
+
+        assertEquals("name1", userBeforeUpdate.getName());
+    }
+
+    @Test
+    void updateUser_emailIsBlank_notUpdate() {
+        UserDto user = service.createNewUser(user1);
+
+        UserDto updateUser = user1;
+        updateUser.setEmail("");
+
+        UserDto userBeforeUpdate = service.updateUser(updateUser, updateUser.getId());
+
+        assertEquals(user.getEmail(), userBeforeUpdate.getEmail());
+    }
+
+    @Test
+    void updateUser_emailIsNull_notUpdate() {
+        UserDto user = service.createNewUser(user1);
+
+        UserDto updateUser = user1;
+        updateUser.setEmail(null);
+
+        UserDto userBeforeUpdate = service.updateUser(updateUser, updateUser.getId());
+
+        assertEquals(user.getEmail(), userBeforeUpdate.getEmail());
+    }
+
+    @DirtiesContext
+    @Test
+    void updateUser_emailIsExist_expectedError() {
+        UserDto userOne = service.createNewUser(user1);
+        UserDto userDto = new UserDto(2L, "name2", "emai@mail.cin");
+        UserDto userTwo = service.createNewUser(userDto);
+        userTwo.setEmail(userOne.getEmail());
+
+        assertThrows(Exception.class, () -> service.updateUser(userTwo, userTwo.getId()));
+    }
+
+    @Test
+    void updateUser_allNull_returnNull() {
+        UserDto user = service.createNewUser(user1);
+
+        UserDto updateUser = user1;
+        updateUser.setEmail(null);
+        updateUser.setName(null);
+
+        UserDto userBeforeUpdate = service.updateUser(updateUser, updateUser.getId());
+
+        assertNull(userBeforeUpdate);
+    }
+
+    @Test
     void updateUserNameInContext_expectedCorrect_returnUserDtoBeforeUpdate() {
         service.createNewUser(user1);
 
@@ -51,6 +125,7 @@ public class UserServiceWithcContext {
 
         assertEquals(updateUser.getName(), userBeforeUpdate.getName(), "Name not update");
     }
+
 
     @Test
     void deleteUser_correct() {
