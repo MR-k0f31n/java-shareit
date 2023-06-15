920c7e46aadcf12644a4b8018e7f3e6e8862dd5c
MR-k0f31n <bukalov_igor@mail.ru>
2023-06-15 09:55:54 +0300

    <TO DO> split program on two microservice - gateway and server.

--- server/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.comment.CommentDto;
@@ -30,7 +31,7 @@ public class ItemController {
                                             @RequestParam Integer size,
                                             @RequestHeader("X-Sharer-User-Id") Long userId) {
         log.debug("Endpoint request: 'GET /items'");
-        final Pageable pageable = PageRequest.of(from / size, size);
+        final Pageable pageable = PageRequest.of(from / size, size, Sort.by("id"));
         return service.getAllItemsByOwner(userId, pageable);
     }
 
